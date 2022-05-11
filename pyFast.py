56f7f449fdac960eaf555ae040dc25dffbecb72b
ArneWi11 <arne.widderich@stud.dhsh.de>
2022-05-11 11:38:58 +0200

    SPEEEEED

---
+++ pyFast.py
@@ -0,0 +1,32 @@
+import datetime
+import mysql.connector
+from mysql.connector import errorcode
+
+try:
+       cnx = mysql.connector.connect(user='newuser', password='12Ab$%678',
+                              host='127.0.0.1',
+                              database='employees')
+except mysql.connector.Error as err:
+  if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
+    print("Something is wrong with your user name or password")
+  elif err.errno == errorcode.ER_BAD_DB_ERROR:
+    print("Database does not exist")
+  else:
+    print(err)
+else:
+  cursor = cnx.cursor()
+
+query = ("SELECT first_name, last_name, hire_date FROM employees "
+         "WHERE hire_date BETWEEN %s AND %s")
+
+hire_start = datetime.date(1999, 1, 1)
+hire_end = datetime.date(1999, 12, 31)
+
+cursor.execute(query, (hire_start, hire_end))
+i=0
+for (first_name, last_name, hire_date) in cursor:
+    #print("{}, {} was hired on {:%d %b %Y}".format(
+    #last_name, first_name, hire_date))
+    i += 1
+print("Number of Rows =", i)
+cursor.close()
